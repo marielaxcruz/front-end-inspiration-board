50ce1d5d559bc1d2dbb62f8aac92d4e334b92140
marielaxcruz <marielacrtzcruz@gmail.com>
2021-06-30 12:08:46 -0700

    added new changes to forms

--- src/components/NewBoardForm.js
+++ src/components/NewBoardForm.js
@@ -1,1 +1,34 @@
 import React, { useState } from 'react';
+
+const NewBoardForm = (props) => {
+    const [title, setTitle] = useState('');
+
+    const onInputChange = (event) => {
+        console.log(event);
+        console.log(event.target.name);
+        setTitle(event.target.value);
+    }
+
+    const onSubmit = (event) => {
+      // Prevent the browser submitting form
+      // and reloading the page
+        event.preventDefault();
+      // POST 'localhost:3000'
+        if (title !== '') {
+            props.onSubmitCallback(title);
+        }
+    }
+
+    return (
+    <form onSubmit={onSubmit} >
+        <label>Title</label>
+        <input name="title-input" id="title-input" value={title} onChange={onInputChange} />
+        <label>Owner's Name </label>
+        <input name="owner-input" id="owner-input" value={title} onChange={onInputChange} />
+        <button>Submit</button>
+        <input type="submit" />
+    </form>
+    )
+}
+
+export default NewBoardForm;
