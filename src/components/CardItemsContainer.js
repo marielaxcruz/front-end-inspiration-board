d7689266e3b9ee8086a3894d483ad675bc820a59
sneha agarwal <sneha8412@gmail.com>
2021-06-30 12:45:24 -0700

    card skeleton working

--- src/components/CardItemsContainer.js
+++ src/components/CardItemsContainer.js
@@ -4,13 +4,9 @@ import CardItem from './CardItem';
 //import './CardItemsContainer.css'
 
 const CardItemsContainer = (props) => {
-    
-    //const cardList = generateCardComponents(props.cards);
-    //console.log('1-d square list', squareList);
-    
+     
     const cardItems = props.cards.map((card) => {
-      //console.log('sq-value:', sq.value);
-      //console.log('sq-id:', sq.id);
+     
       return (
          <CardItem id={card.id} message={card.message} likeCount={card.likeCount}></CardItem>
        );
