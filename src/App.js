2342e3176e8ec76db75b1085eb576d64176f5a76
sneha agarwal <sneha8412@gmail.com>
2021-06-30 17:03:24 -0700

    updated App.js to show how board name and selectedBoardid should be initialized. Currently using hardcoded values for testing.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import './App.css';
 function App() {
   
   const [currentSelectedBoardId, setSelectedBoardId] = useState(-1); // -1 is the initial state, not using 0 because 0 is a valid boardid
-  
+  const [currentBoardName, setBoardName] = useState("");
+
   return (
     <div className="App">
       {/* <BoardsContainer /> */}
-      {/* <CardsContainer boardName="Sneha's board" selectedBoardId={currentSelectedBoardId} /> */}
+      {/* <CardsContainer boardName={currentBoardName} selectedBoardId={currentSelectedBoardId} /> */}
       <CardsContainer boardName="Sneha's board" selectedBoardId="1" />
     </div>
   );
