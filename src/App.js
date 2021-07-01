22ea757bdb5a49a24af46bc8abaa94e5597e1ce9
sneha agarwal <sneha8412@gmail.com>
2021-06-30 17:15:09 -0700

    Added onSelectedBoardCallback handler in App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,17 @@ function App() {
   const [currentSelectedBoardId, setSelectedBoardId] = useState(-1); // -1 is the initial state, not using 0 because 0 is a valid boardid
   const [currentBoardName, setBoardName] = useState("");
 
+  const onBoardSelectedCallback = (selectedBoardId, selectedBoardName) => {
+    // Set the state for the selected board so the CardContainer can use it to behave correctly.
+    setBoardName(selectedBoardName);
+    setSelectedBoardId(selectedBoardId);
+  }
+
   return (
+    // UnComment the commented lines below in the final app
+
     <div className="App">
-      {/* <BoardsContainer /> */}
+      {/* <BoardsContainer onBoardSelectedCallback={onBoardSelectedCallback} /> */}
       {/* <CardsContainer boardName={currentBoardName} selectedBoardId={currentSelectedBoardId} /> */}
       <CardsContainer boardName="Sneha's board" selectedBoardId="1" />
     </div>
