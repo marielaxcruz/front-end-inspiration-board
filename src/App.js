b073bdbabcac53191a3f10c332a27dad8b4c0b0f
ggrossvi <hello.gloria@gmail.com>
2021-07-01 10:41:12 -0700

    hooked up to api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+
+
+//import Board from "./components/Board";
+//import Cards from "./components/Cards";
+import axios from 'axios';
+// var log = require('console-log-level')({ level: 'info' });
+//add the appropriate sytax for <board>
+
+
+ {/*
+function Axios(){
+        axios.get(URL,
+        { //not sure if we need values for get
+          params: {
+            board_id:'',
+            card_id: '',
+          }
+
+        })
+        .then((response) => {
+          console.log('success!');
+          console.log(response) //response.data
+        })
+        .catch((error) => {
+          console.log('failed!');
+          console.log(error)  //error.response.data
+        });
+       
+        .finally(() => {
+          not sure if we need to do something here to close our database?
+        })
+       
+
+        const cardData = {
+          message: ''
+        }
+        axios
+        .post('/card', cardData)
+       
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+}
+ */}
+const App = () => {
+  // [board,setBoard] = useState(generateBoard());
+  // [cards,setCards] = useState(generateCards());
+  //const [currentSelectedBoardId, setselectedBoardId] = useState(-1);
+  const [errorMessage, setErrorMessage] = useState('');
+  const URL = "http://localhost:5000/board";
+  // let [responseData, setResponseData] = useState('');
+
+  useEffect(() => {
+    console.log(URL)
+    axios.get(URL)
+      .then ((response) => {
+        console.log(response.data);
+       
+      })
+      .catch((error) => {
+        setErrorMessage(
+          console.log(error)
+        
+        );
+      });
+  }, []);  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {errorMessage}
+      {/*<BoardsContainer/>
+      <CardsContainer boardName = "Sneha's board" 
+      selectedBoardID={currentSelectedBoardID}/>
+      */}
     </div>
   );
 }
