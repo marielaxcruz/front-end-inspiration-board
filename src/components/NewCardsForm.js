d77e0fa05767e8d6b36abe782c79c4242345ed1c
sneha agarwal <sneha8412@gmail.com>
2021-07-01 18:16:45 -0700

    update NewCardForm to auto disable submit button based on message length

--- src/components/NewCardsForm.js
+++ src/components/NewCardsForm.js
@@ -5,10 +5,21 @@ import './NewCardsForm.css'
 
 const NewCardForm = (props) => {
     const [message, setMessage] = useState('');
+    const [submitButtonDisableState, setSubmitButtonDisableState] = useState("disabled");
 
     const onInputChange = (event) => {
-        console.log(event);
-        console.log(event.target.name);
+        // console.log(event);
+        // console.log(event.target.name);
+
+        const messageText = event.target.value;
+        
+        if (messageText.length > 5 || messageText.length === 0){
+            setSubmitButtonDisableState(true);
+        }
+        else{
+            setSubmitButtonDisableState(false);
+        }
+
         setMessage(event.target.value);
     }
 
@@ -27,7 +38,7 @@ const NewCardForm = (props) => {
         <label>Message</label>
         <input name="message-input" id="message-input" value={message} onChange={onInputChange} />
         {/*<button>Submit</button>*/}
-        <input type="submit" />
+        <input type="submit" disabled={submitButtonDisableState}/>
     </form>
     )
 }
